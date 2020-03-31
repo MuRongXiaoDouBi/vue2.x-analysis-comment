47fb84d903469fc41593cf6dd9d794cfc949638e
MuRong <372638156@qq.com>
2020-03-31 18:22:00 +0800

    newVue发生了什么

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -5,15 +5,19 @@ import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 
-function Vue (options) {
+function Vue(options) {
+  // 判断this是否是Vue实例
+  // 如果不是则抛出警告
+  // 如果是则初始化Vue配置
   if (process.env.NODE_ENV !== 'production' &&
     !(this instanceof Vue)
   ) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+  // 初始化Vue配置
   this._init(options)
 }
-
+// 合并配置、初始化生命周期、初始化事件中心、初始化渲染、初始化data、props、computed、watcher等
 initMixin(Vue)
 stateMixin(Vue)
 eventsMixin(Vue)
--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -14,58 +14,73 @@ let uid = 0
 
 export function initMixin (Vue: Class<Component>) {
   Vue.prototype._init = function (options?: Object) {
+    // 赋值vue实例给vm变量
     const vm: Component = this
-    // a uid
+    // a uid vm实例唯一标识
     vm._uid = uid++
 
     let startTag, endTag
-    /* istanbul ignore if */
+    /* istanbul ignore if 性能统计相关 */
     if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
       startTag = `vue-perf-start:${vm._uid}`
       endTag = `vue-perf-end:${vm._uid}`
       mark(startTag)
     }
 
-    // a flag to avoid this being observed
+    //  a flag to avoid this being observed 监听对象变化时用于过滤vm
     vm._isVue = true
     // merge options
+    // 合并配置
+    // _isComponent是内部创建子组件时才会添加为true的属性
     if (options && options._isComponent) {
-      // optimize internal component instantiation
-      // since dynamic options merging is pretty slow, and none of the
-      // internal component options needs special treatment.
+      // optimize internal component instantiation 优化内部组件实例化
+      // since dynamic options merging is pretty slow, and none of the 因为动态配置合并非常慢，而且没有
+      // internal component options needs special treatment. 内部组件配置需要特殊处理。
+      // 初始化内部组件
       initInternalComponent(vm, options)
     } else {
+      // mergeOptions 合并构造函数及构造函数父级上定义的options
       vm.$options = mergeOptions(
         resolveConstructorOptions(vm.constructor),
         options || {},
         vm
       )
     }
-    /* istanbul ignore else */
+    /* istanbul ignore else 性能统计相关*/
     if (process.env.NODE_ENV !== 'production') {
       initProxy(vm)
     } else {
       vm._renderProxy = vm
     }
-    // expose real self
+    // 暴露出真实的实例
     vm._self = vm
+    // 初始化生命周期
     initLifecycle(vm)
+    // 初始化事件中心
     initEvents(vm)
+    // 初始化渲染
     initRender(vm)
+    // 调用beforeCreate生命周期钩子
     callHook(vm, 'beforeCreate')
+    // 注入data/props之前需要做的
     initInjections(vm) // resolve injections before data/props
+    // 注入data/props
     initState(vm)
+    // 注入data/props之后需要做的
     initProvide(vm) // resolve provide after data/props
+    // 调用created生命周期钩子
     callHook(vm, 'created')
 
-    /* istanbul ignore if */
+    /* istanbul ignore if 性能统计相关*/
     if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
       vm._name = formatComponentName(vm, false)
       mark(endTag)
       measure(`vue ${vm._name} init`, startTag, endTag)
     }
-
+    // 判断$option里有没用传入el
     if (vm.$options.el) {
+      // 挂载DOM节点
+
       vm.$mount(vm.$options.el)
     }
   }
